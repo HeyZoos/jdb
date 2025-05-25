afe47de7f5644cdce328923b0899aa9edf318521
Jesus Bracho <jessebracho@gmail.com>
2025-05-24 22:50:22 -0400

    feat: read user args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use tracing::error;
+
 fn main() {
-    println!("Hello, world!");
+    tracing_subscriber::fmt::init();
+    
+    let args = std::env::args();
+
+    if args.len() == 1 {
+        error!("No arguments provided");
+        std::process::exit(-1);
+    }
 }
