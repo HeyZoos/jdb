9abfd44e62e06f6668e8fa1159db4d0161ff1b61
Jesus Bracho <jessebracho@gmail.com>
2025-05-24 23:10:52 -0400

    refactor: use clap

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,30 @@
+use clap::ArgGroup;
+use clap::Parser;
 use tracing::error;
 
 fn main() {
+    // Install a global tracing subscriber that listens for events and filters based on the value of
+    // the RUST_LOG environment variable. This is a quick and easy way to get the loggers to just
+    // **do stuff**.
     tracing_subscriber::fmt::init();
-    
-    let args = std::env::args();
 
-    if args.len() == 1 {
-        error!("No arguments provided");
-        std::process::exit(-1);
-    }
+    // Get the program invocation arguments.
+    let args = Args::parse();
+}
+
+#[derive(Debug, Parser)]
+#[command(about, version, group(
+    ArgGroup::new("target")
+        .required(true)
+        .multiple(false)
+        .args(["program", "pid"])
+))]
+struct Args {
+    // We can possibly kick off this child as a subprocess i.e. `jdb --program <program>`
+    #[arg(long)]
+    program: Option<String>,
+
+    // Or we can attach to a running PID `jdb --pid <pid>`
+    #[arg(long)]
+    pid: Option<u32>,
 }
