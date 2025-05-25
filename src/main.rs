98610bbac1bcb61062966b784122c16e0585ddd6
Jesus Bracho <jessebracho@gmail.com>
2025-05-25 00:19:17 -0400

    feat: wait for child process to pause

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@ use clap::ArgGroup;
 use clap::Parser;
 use nix::sys::ptrace;
 use nix::unistd::Pid;
-use nix::{sys::wait::waitpid, unistd::{fork, ForkResult}};
-use tracing::{info};
+use nix::{
+    sys::wait::waitpid,
+    unistd::{ForkResult, fork},
+};
 use std::str::FromStr;
+use tracing::info;
 
 /// ```sh
 /// cargo run -- --program /bin/echo
@@ -18,32 +21,52 @@ fn main() {
     // Get the program invocation arguments.
     let args = Args::parse();
 
-    // Attach to running program or spawn program.
+    let mut target: Option<Pid> = None;
+
+    // Attach to running program.
     if let Some(pid) = args.pid {
-        info!(target=pid, pid=std::process::id(), "Attaching to target process");
-        ptrace::attach(Pid::from_raw(pid)).unwrap();
+        info!(
+            target = pid,
+            pid = std::process::id(),
+            "Attaching to target process"
+        );
+        target = Some(Pid::from_raw(pid));
+        ptrace::attach(target.unwrap()).unwrap();
     }
 
+    // Or attempt to create and attach to a child program.
     if let Some(program) = args.program {
-        match unsafe{fork()} {
+        match unsafe { fork() } {
             Ok(ForkResult::Parent { child, .. }) => {
-                info!(child=child.as_raw(), pid=std::process::id(), "Created child process");
-                waitpid(child, None).unwrap();
+                info!(
+                    child = child.as_raw(),
+                    pid = std::process::id(),
+                    "Created child process"
+                );
+                target = Some(child);
             }
             Ok(ForkResult::Child) => {
                 // Indicates that this process is to be traced by its parent.
                 // This is the only ptrace request to be issued by the tracee.
                 // https://docs.rs/nix/latest/nix/sys/ptrace/fn.traceme.html
-                info!(pid=std::process::id(), "Asking to be traced");
+                info!(pid = std::process::id(), "Asking to be traced");
                 ptrace::traceme().unwrap();
-                info!(pid=std::process::id(), program, "Calling exec");
+                info!(pid = std::process::id(), program, "Calling exec");
                 let program_cstring = std::ffi::CString::from_str(program.as_str()).unwrap();
                 // Conventionally, argv[0] is the program name.
+                // TODO: We need to imitate the path aware behavior of `execlp`
                 nix::unistd::execv::<_>(&program_cstring, &vec![program_cstring.clone()]).unwrap();
             }
             Err(_) => println!("Fork failed"),
         }
     }
+
+    info!(
+        child = target.unwrap().as_raw(),
+        pid = std::process::id(),
+        "Waiting for child process to pause"
+    );
+    waitpid(target.unwrap(), None).unwrap();
 }
 
 #[derive(Debug, Parser)]
